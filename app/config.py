9aaeacc00e724a07ba82bcbf5f8e813f85970cb9
nedoder <nedoder89@gmail.com>
2024-07-01 18:44:11 +0200

    fixing config file

--- app/config.py
+++ app/config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    POSTGRES_USER : str= os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD : str= os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_DB : str= os.getenv("POSTGRES_DB")
-    POSTGRES_PORT : str= os.getenv("POSTGRES_PORT")
-    DATABASE_URL: str = f"postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:${POSTGRES_PORT}/${POSTGRES_DB}"
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
+    DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 settings = Settings()
